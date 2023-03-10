304ac8366e4029dd599ac971f53a40e247b25b9a
m-kumar22 <kumarmanas391@gmail.com>
2023-03-11 05:23:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,155 @@
+function closeall(){
+    document.querySelector(".description").style.display="none";
+}
+
+var flag1=0;
+var flag2=0;
+var flag3=0;
+var flag4=0;
+var flag5=0;
+var flag6=0;
+
+document.querySelector("#q1").addEventListener("click",function(){
+    closeall();
+    if(flag1===0)
+    {
+        flag1=1;
+        document.querySelector('#d1').style.display="block";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+    else
+    {
+        flag1=0;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+})
+
+document.querySelector("#q2").addEventListener("click",function(){
+    closeall();
+    if(flag2===0)
+    {
+        flag2=1;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="block";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+    else
+    {
+        flag2=0;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+})
+
+document.querySelector("#q3").addEventListener("click",function(){
+    closeall();
+    if(flag3===0)
+    {
+        flag3=1;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="block";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+    else
+    {
+        flag3=0;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+})
+
+document.querySelector("#q4").addEventListener("click",function(){
+    closeall();
+    if(flag4===0)
+    {
+        flag4=1;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="block";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+    else
+    {
+        flag4=0;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+})
+
+document.querySelector("#q5").addEventListener("click",function(){
+    closeall();
+    if(flag5===0)
+    {
+        flag5=1;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="block";
+        document.querySelector('#d6').style.display="none";
+    }
+    else
+    {
+        flag5=0;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+})
+
+document.querySelector("#q6").addEventListener("click",function(){
+    closeall();
+    if(flag2===0)
+    {
+        flag2=1;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="block";
+    }
+    else
+    {
+        flag2=0;
+        document.querySelector('#d1').style.display="none";
+        document.querySelector('#d2').style.display="none";
+        document.querySelector('#d3').style.display="none";
+        document.querySelector('#d4').style.display="none";
+        document.querySelector('#d5').style.display="none";
+        document.querySelector('#d6').style.display="none";
+    }
+})
+
